8ab4645a75a7b75f4fd8ceae2163e55d356f9595
daniel martins <freitasdaniel39@gmail.com>
2021-01-26 21:30:43 -0300

    aula 2 - linter e router

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
-
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -26,26 +22,46 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz - css</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
-            <Widget.Header>
-              <h1>asasasasas</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>oalasdass</p>
-            </Widget.Content>
+          <Widget.Header>
+            <h1>asasasasas</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Digite seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
+          </Widget.Content>
         </Widget>
         <Widget>
-            <Widget.Content>
-              <p>oalasdass</p>
-            </Widget.Content>
+          <Widget.Content>
+            <p>oalasdass</p>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
       <GitHubCorner />
     </QuizBackground>
-  )
+  );
 }
